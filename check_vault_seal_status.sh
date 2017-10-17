441ff373b8ede1626f5545091fac914536a6d3f3
sashman <sashman90@gmail.com>
2017-10-17 06:45:49 -0400

    Minor updates

--- check_vault_seal_status.sh
+++ check_vault_seal_status.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Assumes the following:
-# - The `curl` package is installed
+# - The `curl`, `jq` package is installed
 # - Vault is listening on the standard port 8200
 # - Vault is using https with a valid certificate
 
 # The hostname of the vault server that we're supposed to check.
-hostname=$1
+# Example value: https://localhost:8200
 
-if [ -z "$hostname" ] ; then
+address=$1
+
+if [ -z "$address" ] ; then
     echo "Usage: check_vault_seal vault_server_hostname"
     exit 3
 fi
 
 # Fetch the vault status in json format.
-json=`curl -s https://${1}:8200/v1/sys/seal-status`
+json=`curl -s ${1}/v1/sys/seal-status`
 
 # Did we succeed in fetching the vault status?
 if [ $? == 0 ] ; then
